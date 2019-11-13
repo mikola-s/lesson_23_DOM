bb7651342bcc583038da567a350afecd084a535a
mikola-s <suharev.nv@gmail.com>
2019-11-13 20:45:23 +0200

    lsn23 1.05 task complete

--- 1.05/app.js
+++ 1.05/app.js
@@ -1,2 +1,19 @@
-// 1.05. Нужно взять и сделать функцию getInnerTextOfElement, которая принимает в качестве аргумента название например '#name'. То внутри делает поиск по getElementById и берет innerText и возварашает. НЕ МОЖНО использовать querySelector. Првоверяете аргумент. Начинается на точку или на #, чтобы вызвать тот метод, который вам нужен.
+// 1.05. Нужно взять и сделать функцию getInnerTextOfElement,
+// которая принимает в качестве аргумента название например '#name'.
+// То внутри делает поиск по getElementById и берет innerText и возварашает.
+// НЕ МОЖНО использовать querySelector. Првоверяете аргумент. Начинается на точку или на #,
+// чтобы вызвать тот метод, который вам нужен.
 
+// добавление нескольких тегов с ID
+let arr = ['tag', 'any_tag', 'some_tag', 'random_tag', 'more_tag',].map( (val, index) => {
+    document.body.appendChild(document.createElement(val)).id = `tag${index}`
+    document.getElementById(`tag${index}`).innerText = `тег "${val}" с ID "tag${index}"\n`
+})
+
+let str = prompt('Пользователь! \n введи ИД тега')
+
+function getInnerTextOfElement(str) {
+    return document.getElementById(str) != null ? document.getElementById(str).innerText : false
+}
+
+alert(getInnerTextOfElement(str))
