50ef3b9e0bf4ce2f6171d556ae63ccefc6ecc280
mikola-s <suharev.nv@gmail.com>
2019-11-13 19:17:06 +0200

    lsn23 1.02 task complete

--- 1.02/app.js
+++ 1.02/app.js
@@ -1,3 +1,21 @@
-// 1.02. Делаем функцию, которая принимает аргументом название тега и возварашает созданый тег. Потом Делаем функцию, которая принимает этот тег как аргумент и вставляет в body.
+// 1.02. Делаем функцию, которая принимает аргументом название тега
+// и возварашает созданый тег.
+// Потом Делаем функцию, которая принимает этот тег как аргумент и вставляет в body.
 // PS. createEl..., append...
 
+let name = prompt('Пользователь! \n введи название тега')
+
+// вариант 1
+{(name != '' && name != null) ? document.body.appendChild(document.createElement(name)) : none}
+
+
+// вариант 2
+function create_tag(name){
+    return (name != '' && name != null) ? document.createElement(name) : false
+}
+
+function insert_tag(tag_name) {
+    return tag_name ? document.body.append(tag_name): false
+}
+
+insert_tag(create_tag(name))
