d0c94abd67a32e3b3de7e50da312a101778c86b8
mikola-s <suharev.nv@gmail.com>
2019-11-14 16:45:54 +0200

    lsn23 2.12 task complete

--- 2.12/app.js
+++ 2.12/app.js
@@ -1,2 +1,8 @@
-2.12. Дан ol. Вставьте ему в конец li с текстом 'HELLO WORLD!'.
+// 2.12. Дан ol. Вставьте ему в конец li с текстом 'HELLO WORLD!'.
 
+
+setTimeout(function () {
+    $('ol li').each(function (_, elem) {
+        elem.append(' HELLO WORLD!')
+    })
+}, 1000)
