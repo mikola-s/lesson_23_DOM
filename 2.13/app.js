1d49033dac5615e3482cc37004d3d425dc45230c
mikola-s <suharev.nv@gmail.com>
2019-11-14 16:53:51 +0200

    lsn23 2.13 task complete

--- 2.13/app.js
+++ 2.13/app.js
@@ -1,2 +1,5 @@
-2.13. Дан элемент ul, а в нем li #myunique. Вставьте перед элементом #myunique новую li с текстом '!!!'.
+// 2.13. Дан элемент ul, а в нем li #myunique. Вставьте перед элементом #myunique новую li с текстом '!!!'.
 
+setTimeout(function () {
+    $('#myunique').before('<li>!!!</li>')
+}, 1000)
