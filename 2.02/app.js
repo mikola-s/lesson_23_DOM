8c52ea79466b5b8ee445769f7fedf4a867530c6f
mikola-s <suharev.nv@gmail.com>
2019-11-14 13:10:59 +0200

    lsn23 2.02 task complete

--- 2.02/app.js
+++ 2.02/app.js
@@ -1,2 +1,7 @@
 // 2.02. Дан ol. Через 3 секунды получите его последнего потомка и удалите его.
 
+setTimeout(function () {
+    $('document').ready(function () {
+        $('ol li:last-child').remove()
+    })
+}, 3000)
