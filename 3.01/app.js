2e00dc81cdfd8e2d30a03d973a505e0bfa6a7102
mikola-s <suharev.nv@gmail.com>
2019-11-14 21:12:14 +0200

    lsn23 3.01 task change

--- 3.01/app.js
+++ 3.01/app.js
@@ -2,35 +2,11 @@
 // по наведению на ссылку в конец атрибута title запишется её текст.
 
 title = $('head title').get(0)
-let counter = 0
 
-$("#a1").mouseover(function () {
-    console.log(counter++)
+$('.url').mouseover(function () {
     title.append(this.innerText)
+    console.log(title.innerText)
 })
 
-$("#a2").mouseover(function () {
-    console.log(counter++)
-    title.append(this.innerText)
-})
 
-$("#a3").mouseover(function () {
-    console.log(counter++)
-    title.append(this.innerText)
-})
-
-$("#a4").mouseover(function () {
-    console.log(counter++)
-    title.append(this.innerText)
-})
-
-$("#a5").mouseover(function () {
-    console.log(counter++)
-    title.append(this.innerText)
-})
-
-$("#a6").mouseover(function () {
-    console.log(counter++)
-    title.append(this.innerText)
-})
 
