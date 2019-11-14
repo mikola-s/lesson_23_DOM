c6a165ac0bc9d02487e14f343c6e394d9424b998
mikola-s <suharev.nv@gmail.com>
2019-11-14 19:16:37 +0200

    lsn23 3.01 task complete

--- 3.01/app.js
+++ 3.01/app.js
@@ -1,2 +1,36 @@
-3.01. Даны ссылки. Привяжите всем ссылкам событие - каждый раз по наведению на ссылку в конец атрибута title запишется её текст.
+// 3.01. Даны ссылки. Привяжите всем ссылкам событие - каждый раз
+// по наведению на ссылку в конец атрибута title запишется её текст.
+
+title = $('head title').get(0)
+let counter = 0
+
+$("#a1").mouseover(function () {
+    console.log(counter++)
+    title.append(this.innerText)
+})
+
+$("#a2").mouseover(function () {
+    console.log(counter++)
+    title.append(this.innerText)
+})
+
+$("#a3").mouseover(function () {
+    console.log(counter++)
+    title.append(this.innerText)
+})
+
+$("#a4").mouseover(function () {
+    console.log(counter++)
+    title.append(this.innerText)
+})
+
+$("#a5").mouseover(function () {
+    console.log(counter++)
+    title.append(this.innerText)
+})
+
+$("#a6").mouseover(function () {
+    console.log(counter++)
+    title.append(this.innerText)
+})
 
