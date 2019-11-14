540535f5e1da86de3dfda1b44c58bcf23d6b9b07
mikola-s <suharev.nv@gmail.com>
2019-11-14 12:53:50 +0200

    lsn23 2.01 task complete

--- 2.01/app.js
+++ 2.01/app.js
@@ -1,2 +1,16 @@
-// 2.01. Высота body 4000px; Через каждые 3 секунды плавно крутите "ползунок" вниз и так же через секунду вверх. И так бесконечно раз
+// 2.01. Высота body 4000px; Через каждые 3 секунды плавно крутите "ползунок" вниз
+// и так же через секунду вверх. И так бесконечно раз
 
+
+document.body.style.height = '4000px'
+let counter = 0
+
+$('document').ready(function() {
+    setInterval(function () {
+        console.log(counter++)
+        $("html, body").animate({ scrollTop: 4000 }, 1000)
+        setTimeout(function () {
+            $("html, body").animate({ scrollTop: 0 }, 1000)
+        }, 1000)
+    }, 3000)
+})
