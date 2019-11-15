1ebb4e2ed1f7bc671d07e3c6d743a059c513167d
mikola-s <suharev.nv@gmail.com>
2019-11-15 11:32:34 +0200

    lsn23 3.03 task complete

--- 3.03/app.js
+++ 3.03/app.js
@@ -1,2 +1,18 @@
-3.03. 5 ссылок как и в задаче 2. Только после трех кликов на ссылку следует убрать от нее событие, которое добавляет href в конец текста как в предыдушей задачи
+// 3.03. 5 ссылок как и в задаче 2.
+// Только после трех кликов на ссылку следует убрать от нее событие,
+// которое добавляет href в конец текста как в предыдушей задачи
 
+
+$('a').attr("cliCount", 0)
+
+$('a').bind('click', function () {
+    let cliCount = Number($(this).attr("cliCount"))
+    if (cliCount < 3) {
+        this.append($(this).attr("href"))
+        $(this).attr("cliCount", cliCount + 1)
+        console.log(cliCount + 1)
+    } else {
+        $(this).unbind("click")
+        $(this).removeAttr("cliCount")
+    }
+})
