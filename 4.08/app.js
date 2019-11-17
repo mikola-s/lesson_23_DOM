fd04738c2b2acaa76b96a1683c8584ffd2e4dd35
mikola-s <suharev.nv@gmail.com>
2019-11-17 19:36:45 +0200

    lsn23 4.08 task complete

--- 4.08/app.js
+++ 4.08/app.js
@@ -1,2 +1,25 @@
-4.08. Дан массив с целыми числами. Создайте из него новый массив, где останутся лежать только четные из этих чисел. Для этого используйте вспомогательную функцию isEven из предыдущей задачи.
+// 4.08. Дан массив с целыми числами. Создайте из него новый массив,
+// где останутся лежать только четные из этих чисел.
+// Для этого используйте вспомогательную функцию isEven из предыдущей задачи.
+
+
+let startArr = createRandomNumArray()
+let resultArr = []
+
+$('body').append(`<p>[${startArr.join(', ')}]<\p>`)
+
+startArr.map((val) => !isEven(val) ? resultArr.push(val) : null)
+
+$('body').append(`<p>[${resultArr.join(', ')}]<\p>`)
+
+
+function isEven(num){
+    return num % 2 ? true : false
+}
+
+
+function createRandomNumArray(arrayLength=10, maxInt=100, minInt=-100) {
+    let getRndIntInRange = (max, min=0) => Math.floor(Math.random() * (max - min + 1) + min)
+    return new Array(arrayLength).fill(0).map( () => getRndIntInRange(maxInt, minInt))
+}
 
