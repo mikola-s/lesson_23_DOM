2d2c821f0c5f7c5fb1abdc381113db0f76cbe354
mikola-s <suharev.nv@gmail.com>
2019-11-17 12:53:04 +0200

    lsn23 4.04 task complete

--- 4.04/app.js
+++ 4.04/app.js
@@ -1,2 +1,20 @@
-4.04. Сделайте функцию getDigitsSum (digit - это цифра), которая параметром принимает целое число и возвращает сумму его цифр.
+// 4.04. Сделайте функцию getDigitsSum (digit - это цифра),
+// которая параметром принимает целое число и возвращает сумму его цифр.
+
+let num = random(100000000000, 10000000000)
+
+$('body').append(`<p>${num}<\p>`)
+
+$('body').append(`<p>${getDigitsSum(num)}<\p>`)
+
+
+function getDigitsSum(num, summ=0) {
+    num.toString().split('').map((str)=>Number(str)).map((val) => summ += val)
+    return summ
+}
+
+
+function random(max, min = 0) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
 
