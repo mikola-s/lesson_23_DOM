7682a9b4fbe531333a5b185259d900c972245b6f
mikola-s <suharev.nv@gmail.com>
2019-11-17 14:25:49 +0200

    lsn23 4.07 task complete

--- 4.07/app.js
+++ 4.07/app.js
@@ -1,2 +1,24 @@
-4.07. Сделайте функцию getDivisors, которая параметром принимает число и возвращает массив его делителей (чисел, на которое делится данное число).
+// 4.07. Сделайте функцию getDivisors, которая параметром принимает число
+// и возвращает массив его делителей (чисел, на которое делится данное число).
+
+
+let getRndIntInRange = (max, min = 0) => Math.floor(Math.random() * (max - min + 1) + min)
+let num = getRndIntInRange(1000, -1000)
+
+
+$('body').append(`<p>${num}<\p>`)
+
+$('body').append(`<p>[${getDivisors(num).join(', ')}]<\p>`)
+
+
+function getDivisors(num) {
+    let resultArr = []
+    let dvsr
+    for (num > 0 ? dvsr = 1 : dvsr = -1;
+         num > 0 ? dvsr <= num : dvsr >= num;
+         num > 0 ? dvsr++ : dvsr--) {
+        num % dvsr ? null : resultArr.push(dvsr)
+    }
+    return resultArr
+}
 
